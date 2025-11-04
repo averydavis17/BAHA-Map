498e0b69b6e87ede4d5bb457386ae8886acf631e
averydavis17 <averydavis@berkeley.edu>
2025-11-03 18:31:58 -0800

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,48 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiYXZlcnlkYXZpcyIsImEiOiJjbWg5cmp2cGswcjViMm5vb
 const map = new mapboxgl.Map({
   container: 'map', // container ID
   style: 'mapbox://styles/averydavis/cmh9rqtvv00qi01r54itfb3lj', // your Style URL goes here
-  center: [139.656412, 35.720629], // starting position [lng, lat]. Note that lat must be set between -90 and 90
+  center: [-122.27, 37.8], // starting position [lng, lat]. Note that lat must be set between -90 and 90
   zoom: 9 // starting zoom
-    });
+});
+
+map.on('load', function () {
+  map.addSource('points-data', {
+    type: 'geojson',
+    data: 'https://raw.githubusercontent.com/averydavis17/BAHA-Map/refs/heads/main/map%20(2).geojson'
+  });
+
+  map.addLayer({
+    id: 'points-layer',
+    type: 'circle',
+    source: 'points-data',
+    paint: {
+      'circle-color': '#4264FB',
+      'circle-radius': 6,
+      'circle-stroke-width': 2,
+      'circle-stroke-color': '#ffffff'
+    }
+  });
+
+  map.on('click', 'points-layer', (e) => {
+          const coordinates = e.features[0].geometry.coordinates.slice();
+          const properties = e.features[0].properties;
+
+          const popupContent = `
+            <div>
+                <h3>${properties.Landmark}</h3>
+                <p><strong>Address:</strong> ${properties.Address}</p>
+                <p><strong>Architect & Date:</strong> ${properties.Architect_Date}</p>
+                <p><strong>Designated:</strong> ${properties.Designated}</p>
+                ${properties.Link ? `<p><a href="${properties.Link}" target="_blank">More Information</a></p>` : ''}
+                ${properties.Notes ? `<p><strong>Notes:</strong> ${properties.Notes}</p>` : ''}
+            </div>
+        `;
+
+        new mapboxgl.Popup()
+            .setLngLat(coordinates)
+            .setHTML(popupContent)
+            .addTo(map);
+
+  });
+
+});
