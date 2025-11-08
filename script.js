0463a88e1ca37e8ddf3370fe33d11a3489153a5b
averydavis17 <averydavis@berkeley.edu>
2025-11-08 00:03:30 -0800

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiYXZlcnlkYXZpcyIsImEiOiJjbWg5cmp2cGswcjViMm5vb
 const map = new mapboxgl.Map({
   container: 'map', // container ID
   style: 'mapbox://styles/averydavis/cmh9rqtvv00qi01r54itfb3lj', // your Style URL goes here
-  center: [-122.27, 37.8], // starting position [lng, lat]. Note that lat must be set between -90 and 90
-  zoom: 9 // starting zoom
+  center: [-122.2727, 37.8715], // starting position [lng, lat]. Note that lat must be set between -90 and 90
+  zoom: 11 // starting zoom
 });
 
 map.on('load', function () {
@@ -17,17 +17,49 @@ map.on('load', function () {
     type: 'circle',
     source: 'points-data',
     paint: {
-      'circle-color': '#4264FB',
+      'circle-color': '#583711',
       'circle-radius': 6,
       'circle-stroke-width': 2,
       'circle-stroke-color': '#ffffff'
     }
   });
 
+   map.addLayer({
+    id: 'points-layer-hover',
+    type: 'circle',
+    source: 'points-data',
+    paint: {
+      'circle-radius': 15,          
+      'circle-color': '#FFD700',     
+      'circle-opacity': 0.5          
+    },
+    filter: ['==', 'Landmark', '']   // start hidden
+  });
+
+  // Show the glow on hover
+  map.on('mouseenter', 'points-layer', (e) => {
+    map.getCanvas().style.cursor = 'pointer';
+    const name = e.features[0].properties.Landmark;
+    map.setFilter('points-layer-hover', ['==', 'Landmark', name]);
+  });
+
+  // Hide the glow when not hovering
+  map.on('mouseleave', 'points-layer', () => {
+    map.getCanvas().style.cursor = '';
+    map.setFilter('points-layer-hover', ['==', 'Landmark', '']);
+  });
+
   map.on('click', 'points-layer', (e) => {
           const coordinates = e.features[0].geometry.coordinates.slice();
           const properties = e.features[0].properties;
 
+      map.flyTo({
+      center: coordinates,
+      zoom: 13,     
+      speed: 0.6,    
+      curve: 1,      
+      essential: true
+    });
           const popupContent = `
             <div>
                 <h3>${properties.Landmark}</h3>
@@ -42,8 +74,7 @@ map.on('load', function () {
         new mapboxgl.Popup()
             .setLngLat(coordinates)
             .setHTML(popupContent)
-            .addTo(map);
-
+            .addTo(map);     
   });
 
 });
